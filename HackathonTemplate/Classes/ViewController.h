b8c38e91dc2e67934092c53a07164ae20ffbe215
Eugene Yee <yujean@gmail.com>
2015-03-04 17:54:23 -0500

    Xcode Project files + Podfile

---
+++ HackathonTemplate/Classes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackathonTemplate
+//
+//  Created by Eugene Yee on 3/4/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HackathonTemplate/Classes/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  HackathonTemplate
+//
+//  Created by Eugene Yee on 3/4/15.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+#pragma mark - View Controller Lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+@end
